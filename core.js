16db713bb09b3a061c177f1984538027dbc3a85b
eush77 <eush77@gmail.com>
2013-07-12 00:02:38 +0400

    Typo

--- core.js
+++ core.js
@@ -1,14 +1,14 @@
-/*****************************************************************************************
-* Syntax conventions:                                                                    *
-* -> indices and powers: "x^2", "x_2";                                                   *
-* -> special symbols: "<=>", "*", "->";                                                  *
-* -> whitespace sequences: "  " denotes em-space, "\ " denotes thin-space;               *
-* -> any correct HTML, including tags and entities.                                      *
-* Notes:                                                                                 *
-* 1) "<=>" and "=" preserve appropriate spacing on both sides;                           *
-* 2) "{}"-groupings in indices and powers are also available;                            *
-* 3) "\" is the escape character, except when it occures before space character in "\ ". *
-*****************************************************************************************/
+/****************************************************************************************
+* Syntax conventions:                                                                   *
+* -> indices and powers: "x^2", "x_2";                                                  *
+* -> special symbols: "<=>", "*", "->";                                                 *
+* -> whitespace sequences: "  " denotes em-space, "\ " denotes thin-space;              *
+* -> any correct HTML, including tags and entities.                                     *
+* Notes:                                                                                *
+* 1) "<=>" and "=" preserve appropriate spacing on both sides;                          *
+* 2) "{}"-groupings in indices and powers are also available;                           *
+* 3) "\" is the escape character, except when it occurs before space character in "\ ". *
+****************************************************************************************/
 
 var submit = function() {
     var code = document.getElementById('editor').value;
