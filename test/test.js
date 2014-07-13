7026c6d6a99269edc082e3551ed862dca8882ed1
eush77 <eush77@gmail.com>
2014-07-14 00:16:27 +0400

    Complete Mocha spec

--- test/test.js
+++ test/test.js
@@ -9,7 +9,9 @@ describe('Core functionality', function () {
     convert('').should.equal('');
   });
 
-  it('should replace indices and powers', function () {
+  it('should treat backslash as an escape character');
+
+  it('should support indices and powers syntax', function () {
     convert('1').should.equal('1');
     convert('2^2').should.equal('2<sup>2</sup>');
     convert('2_2').should.equal('2<sub>2</sub>');
@@ -17,6 +19,18 @@ describe('Core functionality', function () {
     convert('2_{2^2}').should.equal('2<sub>2<sup>2</sup></sub>');
     convert('2_{2^{2}}^2').should.equal('2<sub>2<sup>2</sup></sub><sup>2</sup>');
   });
+
+  it('should preserve newlines');
+});
+
+
+describe('Mathematical bold script', function () {
+  it('should replace symbols with corresponding entities');
+});
+
+
+describe('Abstract and common number sets', function () {
+  it('should replace symbols with corresponding entities');
 });
 
 
@@ -24,6 +38,37 @@ describe('Arithmetic and logical operators', function () {
   it('should replace symbols with corresponding entities', function () {
     convert('1+1-1').should.equal('1+1&minus;1');
   });
+
+  it('should not replace dashes with minuses in compound words');
+});
+
+describe('Comparison relations', function () {
+  it('should replace symbols with corresponding entities');
+  it('should parse equals/tilde sequences greedily');
+});
+
+
+describe('Inference relations and constants', function () {
+  it('should replace symbols with corresponding entities');
+  it('should put appropriate spacing on both sides of inference arrows');
+  it('should leave "TT" and "BB" as is, if found close to an alphanumeric character');
+});
+
+
+describe('Arrows', function () {
+  it('should replace symbols with corresponding entities');
+});
+
+
+describe('"Let" and defining signs', function () {
+  it('should replace symbols with corresponding entities');
+  it('should put a space after "let"');
+});
+
+
+describe('Various symbols', function () {
+  it('should replace symbols with corresponding entities');
+  it('should not replace double "o" with infinity inside words');
 });
 
 
@@ -31,4 +76,16 @@ describe('Dashes', function () {
   it('should parse dash sequences greedily', function () {
     convert('-----').should.equal('&mdash;&ndash;');
   });
+});
+
+
+describe('Quotation marks', function () {
+  it('should replace symbols with corresponding entities');
+  it('should parse lt/gt sequences greedily');
+});
+
+
+describe('Whitespace sequences', function () {
+  it('should support thin-space shorthand');
+  it('should parse whitespace sequences greedily');
 });
