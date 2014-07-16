b2ccc7e80a65f5d21abb05900beb73539dcb5920
eush77 <eush77@gmail.com>
2014-07-16 15:58:59 +0400

    Use Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = function (grunt) {
+  grunt.initConfig({
+    jshint: {
+      options: {
+        jshintrc: true
+      },
+      all: ['*.js', 'test/**/*.js']
+    },
+    mocha: {
+      options: {
+        reporter: 'spec',
+        require: 'should'
+      },
+      all: ['test/**/*.js']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.task.renameTask('mochaTest', 'mocha');
+
+  grunt.registerTask('default', ['jshint', 'mocha']);
+};
--- Makefile
+++
@@ -1,9 +0,0 @@
-all: jshint test
-
-jshint:
-	jshint *.js test
-
-test:
-	mocha
-
-.PHONY: jshint test
