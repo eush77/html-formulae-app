46502ed62f4146535be7c808b2fb43c7c57e20aa
eush77 <eush77@gmail.com>
2014-07-25 22:48:09 +0400

    Load script at the end of <body>

--- src/js/index.js
+++ src/js/index.js
@@ -3,16 +3,18 @@
 var convert = require('./convert');
 
 
-var submit = function () {
-  var code = document.getElementById('editor').value;
-  var html = document.getElementById('preview').innerHTML = convert(code);
-  document.getElementById('source').value = html ? '<p>' + html + '</p>' : '';
-};
+var editor = document.getElementById('editor'),
+    preview = document.getElementById('preview'),
+    source = document.getElementById('source');
 
 
-document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('editor').addEventListener('keyup', submit);
-  document.getElementById('source').addEventListener('click', function () {
-    this.select();
-  });
+editor.addEventListener('keyup', function () {
+  var html = convert(editor.value);
+  preview.innerHTML = html;
+  source.value = html ? '<p>' + html + '</p>' : '';
+});
+
+
+source.addEventListener('click', function () {
+  this.select();
 });
