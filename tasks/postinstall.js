1ee1e30b73f01d0f32f5ddc950097f6cb54dbfb7
Eugene Sharygin <eush77@gmail.com>
2014-09-13 15:27:19 +0400

    Lock version of Ionicons

--- tasks/postinstall.js
+++ tasks/postinstall.js
@@ -5,18 +5,17 @@ var glyphs = require('../glyphs.json');
 var cssCodepoints = require('css-codepoints')
   , zipObject = require('lodash.zipobject')
   , pairs = require('lodash.pairs')
+  , extend = require('extend')
   , template = require('lodash.template');
 
 
 var iconFileTemplate = template('u${codepoint}-${name}.svg');
-var iconSourceTemplate = template('https://raw.githubusercontent.com/driftyco/ionicons'
-                                  + '/master/src/${name}.svg');
 
 (function (grunt) {
   grunt.loadNpmTasks('grunt-curl');
   grunt.config('curl', zipObject(pairs(glyphs.icons).map(function (icon) {
-    icon = zipObject(['name', 'codepoint'], icon);
-    return ['glyphs/src/' + iconFileTemplate(icon), iconSourceTemplate(icon)];
+    icon = extend({name: icon[0]}, icon[1]);
+    return ['glyphs/src/' + iconFileTemplate(icon), icon.url];
   })));
 
   grunt.loadNpmTasks('grunt-svgicons2svgfont');
@@ -31,7 +30,13 @@ var iconSourceTemplate = template('https://raw.githubusercontent.com/driftyco/io
   });
 
   grunt.registerTask('fontcss', 'Generate CSS classes per font glyph.', function () {
-    grunt.file.write('glyphs/ionicons.css', cssCodepoints(glyphs));
+    var css = cssCodepoints(extend({}, glyphs, {
+      icons: zipObject(pairs(glyphs.icons).map(function (icon) {
+        return [icon[0], icon[1].codepoint];
+      }))
+    }));
+
+    grunt.file.write('glyphs/ionicons.css', css);
     grunt.log.ok('CSS generated.');
   });
 
